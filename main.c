9aaa6a2ffe52981b62285aa17404d3eb291ab7c0
randiperdiansyah05 <randiperdiansyah05@gmail.com>
2026-04-01 18:04:43 +0700

    Menambahkan warna pada tampilan antarmuka (UI)

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define RESET   "\033[0m"
+#define BLUE    "\033[1;34m"
+#define GREEN   "\033[1;32m"
+#define YELLOW  "\033[1;33m"
+#define CYAN    "\033[1;36m"
+#define RED     "\033[1;31m"
+    
 void konversiSuhu(){}
 void konversiJarak(){}
 void konversiBerat()
@@ -8,40 +15,55 @@ void konversiBerat()
 
     float kg;
 
-        printf("\n=== Konversi Berat ===\n");
-        printf("Masukkan berat (kg): ");
+        printf(BLUE"============================================\n"RESET);
+        printf(GREEN "      ====     KONVERSI BERAT    ===      \n"RESET);
+        printf(BLUE"============================================\n"RESET);
+        printf(GREEN" MASUKAN BERAT (KG): "RESET);
         scanf("%f", &kg);
         
             if (kg < 0) 
             {
-            printf("Input tidak valid!\n");
+            printf(RED"Input tidak valid!\n"RESET);
             return;
             }
     int pilihan;
         while (1)
         {
-        
-        printf("\nPilih jenis konversi:\n");
-        printf("1. Kilogram ke Gram\n");
+        printf(BLUE"\n=======================\n"RESET);
+        printf(GREEN" PILIH JENIS KONVERSI \n"RESET);
+        printf(BLUE"=======================\n"BLUE);
+        printf(YELLOW"1. Kilogram ke Gram\n");
         printf("2. Kilogram ke Pons\n");
         printf("3. Keduanya\n");
-        printf("Pilih (1-3): ");
+        printf("Pilih (1-3): "RESET);
         scanf("%d", &pilihan);
 
             switch(pilihan) 
             {
                 case 1:
-                    printf("Hasil: %.2f kg = %.2f gram\n", kg, kg * 1000);
+                    printf(BLUE"=================\n"RESET);
+                    printf(GREEN" HASIL KONVERSI \n"RESET);
+                    printf(BLUE"=================\n"BLUE);
+                    printf(GREEN"Hasil: %.2f kg = %.2f gram\n"RESET, kg, kg * 1000);
                     break;
                 case 2:
-                    printf("Hasil: %.2f kg = %.2f pons\n", kg, kg * 10);
+                    printf(BLUE"=================\n"RESET);
+                    printf(GREEN" HASIL KONVERSI \n"RESET);
+                    printf(BLUE"=================\n"BLUE);
+                    printf(GREEN"Hasil: %.2f kg = %.2f pons\n"RESET, kg, kg * 10);
                     break;
                 case 3:
-                    printf("Gram: %.2f g\n", kg * 1000);
-                    printf("Pons: %.2f pons\n", kg * 10);
+                    printf(BLUE"=================\n"RESET);
+                    printf(GREEN" HASIL KONVERSI \n"RESET);
+                    printf(BLUE"=================\n"BLUE);
+                    printf(GREEN"Gram: %.2f g\n", kg * 1000);
+                    printf("Pons: %.2f pons\n"RESET, kg * 10);
                     break;
                 default:
-                    printf("Pilihan tidak valid!\n");
+                    printf(BLUE"=================\n"RESET);
+                    printf(GREEN" HASIL KONVERSI \n"RESET);
+                    printf(BLUE"=================\n"BLUE);
+                    printf(RED"Pilihan tidak valid!\n"RESET);
                     continue;
          
                }
